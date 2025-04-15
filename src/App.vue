6a7445b91a3e57edbdf391acbed395001814a76f
SuviAnnina <suvi.varjoranta@gmail.com>
2025-04-15 13:42:40 +0300

    Draft sessionStorage; Tweak styling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <script setup>
 import YarnManager from './components/YarnManager.vue';
 import P5Grid from './components/P5Grid.vue';
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted, watch } from "vue";
 import PatternImage from './components/PatternImage.vue';
 import KnitModel from './components/KnitModel.vue';
 
+import { getSavedSession, updateSavedSession } from './saveSession';
+import { palette, setPalette } from './colorStore';
+import { grid, setGrid } from './gridStore';
+import { selectedTemplate, setSelectedTemplate } from './templateStore';
 
 const handleBeforeUnload = (event) => {
   event.preventDefault();
   event.returnValue = "";
 };
 
+watch(palette, () => {
+  updateSavedSession({ palette: JSON.parse(JSON.stringify(palette)) });
+})
+
+watch(grid,() => {
+  updateSavedSession({ grid: JSON.parse(JSON.stringify(grid)) });
+})
+
+watch(selectedTemplate, () => {
+  updateSavedSession({ selectedTemplate: selectedTemplate.value.size });
+})
+
 onMounted(() => {
   window.addEventListener("beforeunload", handleBeforeUnload);
+  const saved = getSavedSession();
+  console.log('saved in app.vue; ', saved)
+
+  if (saved && Object.keys(saved).length > 0){
+    if (saved.palette){
+      setPalette(saved.palette);
+    }
+    if (saved.grid){
+      console.log('saved.grid check ')
+      // setGrid(saved.grid)
+    }
+    if (saved.selectedTemplate){
+      console.log('saved.selectedtemplate check')
+      // setSelectedTemplate(saved.selectedTemplate)
+    }
+  }
 });
 
 onUnmounted(() => {
@@ -23,7 +55,7 @@ onUnmounted(() => {
 <template>
   <div class="flex flex-wrap h-screen">
     <!-- Left Side -->
-    <div class="w-full sm:w-full lg:w-full xl:w-1/5 2xl:w-1/6 py-4 px-1">
+    <div class="w-full xl:w-1/5 2xl:w-1/6 py-4 px-1">
       <div class="flex space-x-5 justify-center">
         <YarnManager />
         <P5Grid />
@@ -32,13 +64,18 @@ onUnmounted(() => {
 
     <!-- Right Side -->
     <div
-    class="w-full sm:w-full lg:w-full xl:w-4/5 2xl:w-5/6 py-4 px-2 flex flex-col lg:flex-col 2xl:flex-row lg:space-x-4 2xl:space-x-4">
-
-    <div id="yoke" class="w-full lg:w-3/5 xl:ml-10 2xl:w-3/5">
+      class="w-full xl:w-4/5 2xl:w-5/6 py-4 px-2 flex flex-col xl:flex-col 2xl:flex-row xl:space-x-0 2xl:space-x-4">
+      <!-- Pattern Image -->
+      <div
+        id="yoke"
+        class="w-full 2xl:w-3/5 xl:px-10 lg:px-6 md:px-6 sm:px-4 px-2">
         <PatternImage />
       </div>
 
-      <div class="w-full lg:w-2/5 2xl:w-2/5 mt-4 xl:ml-1 xl:mt-5 2xl:mt-0 flex flex-col" id="renderKnitModel">
+      <!-- Knit Model -->
+      <div
+        id="renderKnitModel"
+        class="w-full 2xl:w-2/5 mt-4 2xl:mt-0 xl:px-10 lg:px-6 md:px-6 sm:px-4 px-2 flex flex-col">
         <KnitModel />
       </div>
     </div>
